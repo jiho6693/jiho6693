2a88d12ff4a9c8c4f8a76f1b242e757d91692a57
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2023-10-26 07:04:38 -0400

    ㅁ

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 window.addEventListener("scroll", function () {
-    const category1 = document.getElementsByClassName("category1")[0]; // Select the first element with class "category1"
-    const scrollY = window.scrollY;
+  const category1 = document.getElementsByClassName("category1")[0]; // Select the first element with class "category1"
+  const scrollY = window.scrollY;
 
-    // Define a scroll threshold (adjust the value as needed)
-    const scrollThreshold = 10;
+  // Define a scroll threshold (adjust the value as needed)
+  const scrollThreshold = 10;
 
-    if (scrollY > scrollThreshold) {
-      // Hide the "category1" div by adding a CSS class
-      category1.classList.add("hidden");
-    } else {
-      // Show the "category1" div by removing the CSS class
-      category1.classList.remove("hidden");
-    }
-  });
-
-
-  function createRaindrop() {
-const raindrop = document.createElement("div");
-raindrop.className = "raindrop";
-raindrop.style.left = `${Math.random() * 100}%`;
-document.getElementById("rain").appendChild(raindrop);
-
-raindrop.addEventListener("animationiteration", () => {
-  // 비방울이 화면 하단에 도달하면 제거
-  raindrop.remove();
+  if (scrollY > scrollThreshold) {
+    // Hide the "category1" div by adding a CSS class
+    category1.classList.add("hidden");
+  } else {
+    // Show the "category1" div by removing the CSS class
+    category1.classList.remove("hidden");
+  }
 });
+
+
+function createRaindrop() {
+  const raindrop = document.createElement("div");
+  raindrop.className = "raindrop";
+  raindrop.style.left = `${Math.random() * 100}%`;
+  document.getElementById("rain").appendChild(raindrop);
+
+  raindrop.addEventListener("animationiteration", () => {
+    // 비방울이 화면 하단에 도달하면 제거
+    raindrop.remove();
+  });
 }
 
 setInterval(createRaindrop, 200); // 새로운 비방울을 일정 간격으로 생성
@@ -46,6 +46,3 @@ function updateClock() {
 setInterval(updateClock, 1000);
 
 updateClock();
-
-
-å
